a1a5ec178ece0dfca5d2444ba14c6c450b59e75c
alex-95 <34754167+alex-95@users.noreply.github.com>
2017-12-21 22:27:06 +0100

    Add files via upload

---
+++ mainnn.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main () {
+   float val;
+   char str[20]="100.2932900";
+   
+   
+   val = atof(str);
+   printf("String value = %s, Float value = %f\n", str, val+2);
+
+   strcpy(str, "tutorialspoint.com");
+   val = atof(str);
+   printf("String value = %s, Float value = %f\n", str, val);
+   val=val/2;
+   printf("String value = %s, Float value = %f\n", str, val);
+
+   return(0);
+}
